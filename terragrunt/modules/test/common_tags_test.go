eaedfffc181cec68fcc0676c354d64c3b475b2de
Tomasz Zaremba <tomasz.zaremba@nearform.com>
2022-09-22 14:40:55 +0200

    [terragrunt] common-tags module test

---
+++ terragrunt/modules/test/common_tags_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommonTagsModule(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../common-tags",
+		Vars: map[string]interface{}{
+			"environment":  "test",
+			"stack_name":   "test-stack-name",
+			"project_name": "test-project-name",
+		},
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	outputTags := terraform.OutputMap(t, terraformOptions, "tags")
+	expectedTags := map[string]string{
+		"Environment":  "test",
+		"ManagedBy":    "terraform",
+		"Organisation": "NearForm",
+		"Project":      "test-project-name",
+		"Stack":        "test-stack-name",
+	}
+	assert.Equal(t, expectedTags, outputTags)
+}
